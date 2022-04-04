a68a0892d311619b4dfc9e826abc58a8ec203031
est-m <estmd@protonmail.com>
2022-04-04 02:01:00 -0400

    v1.1 agregado script a barra de navegación

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,32 @@
 $(document).ready(function(){ 
+
+    //Oculta la barra de navegación al hacer scroll
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("nav-bar").style.top = "0";
+    } else {
+        document.getElementById("nav-bar").style.top = "-40px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
     
-        // Tooltips de la sección "DESTACADOS"    
-        $(function(){
-            $('[data-toggle="tooltip"]').tooltip();
-        });
-        
-        // Alerta al enviar el formulario"
-        $("#btn-1").click(function(){
-            alert("Gacias por preferirnos");
-        });  
-        
-        // Agrega smooth scroll a los links de la barra de navegación
-        $(".nav-link").on('click', function(event) {
-            if (this.hash !== "#") {
-                event.preventDefault();
-                var hash = this.hash;
-        
+    // Tooltips de la sección "DESTACADOS"    
+    $(function(){
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+    
+    // Alerta al enviar el formulario"
+    $("#btn-1").click(function(){
+        alert("Gacias por preferirnos");
+    });  
+    
+    // Agrega smooth scroll a los links de la barra de navegación
+    $(".nav-link").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
             // Animación smooth scroll            
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
